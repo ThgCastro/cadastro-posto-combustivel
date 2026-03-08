eb4925c874c35ae93c3e8152454d38321bb6301d
Thiago Castro <nascimentocastro989@gmail.com>
2026-03-08 11:35:23 -0300

    commit inicial

---
+++ src/main/java/org/thgcastro/cadastroconsultapostocombustivel/CadastroConsultaPostocombustivelApplication.java
@@ -0,0 +1,13 @@
+package org.thgcastro.cadastroconsultapostocombustivel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroConsultaPostocombustivelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroConsultaPostocombustivelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/thgcastro/cadastroconsultapostocombustivel/CadastroConsultaPostocombustivelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thgcastro.cadastroconsultapostocombustivel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroConsultaPostocombustivelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
